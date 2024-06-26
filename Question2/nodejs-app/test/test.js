a491ab0aca64755f8b541ca911815a1c6f614e67
rizkan abadi <rizkanabadi@rizkans-Laptop.local>
2024-06-26 07:45:27 +0700

    add

---
+++ Question2/nodejs-app/test/test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('GET /', () => {
+  it('should return Hello, Heroku!', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal('Hello, Heroku!');
+        done();
+      });
+  });
+});
